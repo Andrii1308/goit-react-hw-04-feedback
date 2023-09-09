b687cdaee6382136d450b21f955fafd288b11919
Andrii1308 <123685813+Andrii1308@users.noreply.github.com>
2023-09-09 18:14:46 +0300

    start

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,14 @@
+import { Button, WrapperButton } from './FeedbackOptions.styled';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <WrapperButton>
+      {options.map(option => (
+        <Button key={option} onClick={() => onLeaveFeedback(option)}>
+          {' '}
+          {option}{' '}
+        </Button>
+      ))}
+    </WrapperButton>
+  );
+};
